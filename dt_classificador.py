95961679e9c04aff62467bc6db19fb49f980bb6d
BiaAS2 <balves.souza14@gmail.com>
2024-12-03 15:21:32 -0300

    bugfix: melhoria no código.
    Co-authored-by: Lucelho Silva <lucelhocristiano28@gmail.com>
    Co-authored-by: LuanaAguilar2 <lalvesaguilar@gmail.com>
    Co-authored-by: Tulio-Junior <tulioinacio90@gmail.com>
    Co-authored-by: Renato Noronha <dev.renato10@gmail.com>

--- dt_classificador.py
+++ dt_classificador.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning, module='sklearn')
 
